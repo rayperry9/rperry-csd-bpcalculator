8a0277d1ae2a5da4ae86a444cb489134a9b88be6
raymond.perry <raymond.perry@workday.com>
2020-11-30 18:12:25 +0000

    updated script for k6 perf testing

--- Tests/bp_perftesting.js
+++ Tests/bp_perftesting.js
@@ -1,11 +1,6 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export let options = {
-    ext: {
-        loadimpact: {
-          projectID: 3516378,
-          }
-         }
   vus: 10,
   duration: '30s',
 };
