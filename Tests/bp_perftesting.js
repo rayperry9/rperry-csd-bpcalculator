d1ef4793a6b860f8081964e7a2629aada09cb091
raymond.perry <raymond.perry@workday.com>
2020-11-30 18:23:59 +0000

    updated script for k6 perf testing

--- Tests/bp_perftesting.js
+++ Tests/bp_perftesting.js
@@ -1,13 +1,14 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-
 export let options = {
+  vus: 10,
+  duration: '30s',
+
   ext: {
     loadimpact: {
       projectID: 3516378,
       // Test runs with the same name groups test runs together
-      vus: 10,
-      duration: '30s',
+      name: "YOUR TEST NAME"
     }
   }
 };
