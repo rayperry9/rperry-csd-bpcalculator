869d34b23bdded27735c3f40237d3fbdafb528a9
raymond.perry <raymond.perry@workday.com>
2020-11-30 17:05:03 +0000

    updated script for k6 perf testing

--- Tests/bp_perftesting.js
+++ Tests/bp_perftesting.js
@@ -1,7 +1,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+export let options = {
+  vus: 10,
+  duration: '30s',
+};
 
 export default function() {
-  http.get('http://rayperry-bpcalculator.azurewebsites.net/');
+  http.get('http://rayperry-qa.azurewebsites.net/');
   sleep(1);
 }
